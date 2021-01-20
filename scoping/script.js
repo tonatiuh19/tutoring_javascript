03175fd955e6ca95c4e2f6fea5656f11925d9f9d
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2021-01-19 23:18:39 -0600

    Update script.js

--- scoping/script.js
+++ scoping/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function calcAge(x){
+    const age = 2020-x;
+    console.log(firstName);
+    
+
+    function printAge(){
+        const output = `${firstName} you are ${age}, born in ${x}`;
+        console.log(output);
+
+        if(x >= 1981 && x <= 1996){
+            var millenial = true;
+            const str = `${firstName} you are a millenial`;
+            console.log(str);
+        }
+        console.log(millenial);
+    }
+    printAge();
+    return age;
+}
+
+const firstName = 'Felix';
+
+calcAge(1993);
